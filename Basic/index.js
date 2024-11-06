6dd0db327ece696a9d89d369bccc8018037a86e7
AniketSharma-dev <aniketsharma.xo@gmail.com>
2024-11-06 20:24:14 +0530

    Basic

--- Basic/index.js
+++ Basic/index.js
@@ -1,21 +1,20 @@
-const http = require("http"); // import http which come with node 
+const http = require("http"); // import http which come with node
 
+const PORT = 8000;
 
 // creating a server with the help of http
-const server = http.createServer((req, res) => { // getting req == request, res == response
-//giving condition for routes
-  if(req.url == "/about"){
+const server = http.createServer((req, res) => {
+  // getting req == request, res == response
+  //giving condition for routes
+  if (req.url == "/about") {
     res.end("The About Page");
-  }
-  else if(req.url == "/profile"){
+  } else if (req.url == "/profile") {
     res.end("The Profile Page");
-  }
-  else{
-    res.end("Hello World!!!")
+  } else {
+    res.end("Hello World!!!");
   }
 });
-//server listen / start at this port 
-server.listen(8000);
-
-
-
+//server listen / start at this port
+server.listen(PORT, () => {
+  console.log(`listning on Server localhost:${PORT} `);
+});
